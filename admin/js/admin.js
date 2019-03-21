f3ab76c3d58f317d5bedcd24119d8900c15a3f74
acebing <1228984759@qq.com>
2019-03-21 23:06:39 +0800

    Update admin.js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,35 +1,35 @@
 // 表单验证
 $(function(){
 	$("#submitBtn").click(function(){
-			if($("#articlecat_form").valid()){
-				$("#articlecat_form").submit();
-			}
-		});
+		if($("#articlecat_form").valid()){
+			$("#articlecat_form").submit();
+		}
+	});
 	
-		$('#articlecat_form').validate({
-			errorPlacement:function(error, element){
-				var error_div = element.parents('div.label_value').find('div.form_prompt');
-				element.parents('div.label_value').find(".notic").hide();
-				error_div.append(error);
+	$('#articlecat_form').validate({
+		errorPlacement:function(error, element){
+			var error_div = element.parents('div.label_value').find('div.form_prompt');
+			element.parents('div.label_value').find(".notic").hide();
+			error_div.append(error);
+		},
+		rules : {
+			name : {
+				required : true
 			},
-			rules : {
-				name : {
-					required : true
-				},
-				sort_order : {
-					required : true
-				}
-					
-			},
-			messages : {
-				name : {
-					required : '<i class="icon icon-exclamation-sign"></i>'+"分类名称必填"
-				},
-				sort_order : {
-					required : '<i class="icon icon-exclamation-sign"></i>'+"分类名称"
-				}
+			sort_order : {
+				required : true
 			}
-		});
+			
+		},
+		messages : {
+			name : {
+				required : '<i class="icon icon-exclamation-sign"></i>'+"分类名称必填"
+			},
+			sort_order : {
+				required : '<i class="icon icon-exclamation-sign"></i>'+"分类名称"
+			}
+		}
+	});
 
 // 点击获取文章分类内容及分类ID
 $('.imitate_select').on('click', function(event) {
@@ -39,7 +39,7 @@ $('.imitate_select').on('click', function(event) {
 	_a.on('click', function(event) {
 		 sel_this.children('div.cite').html($(this).html());// 点击获取分类列表获取内容
 		 sel_this.children('input[type=hidden]').val($(this).attr('data-value'));// 点击获取分类列表获取分类id
-	});
+		});
 });	
 // 点击空白处隐藏展开框
 $(document).click(function(e){
@@ -65,7 +65,7 @@ $(document).click(function(e){
 		$('.imitate_select ul').hide();
 	}
 
-	});
+});
 });
 
 
